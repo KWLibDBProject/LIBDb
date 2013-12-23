a1170c68993aa4f93885843523b2890525f56e2f
karelwintersky <karel.wintersky@gmail.com>
2013-12-23 04:16:39 +0400

    core ready

--- www/core/config.php
+++ www/core/config.php
@@ -1,16 +1,17 @@
 <?php
 // здесь задаются ключевые переменные конфигурации (доступ к базе)
+
 $CONFIG['username_local'] 	= 'root';
-$CONFIG['username_remote']	= '';
+$CONFIG['username_remote']	= 'root';
 
 $CONFIG['host_local']    	= 'localhost';
-$CONFIG['host_remote']   	= '';
+$CONFIG['host_remote']   	= 'localhost';
 
 $CONFIG['password_local']	= '';
-$CONFIG['password_remote']	= '';
+$CONFIG['password_remote']	= 'JTofv6iB';
 
 $CONFIG['database_local']       = 'libdb';
-$CONFIG['database_remote']       = '';
+$CONFIG['database_remote']       = 'libdb';
 
 
 ?>
