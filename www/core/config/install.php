c5de19e3a324eaf239bfa7ca154f2e964405b7ec
karelwintersky <karel.wintersky@gmail.com>
2014-01-09 20:31:16 +0400

    важное исправление: инсталл

--- www/core/config/install.php
+++ www/core/config/install.php
@@ -9,20 +9,21 @@ $r_root = mysql_query($q_root);
 $tpl = new kwt('install.tpl');
 $tpl->contentstart();
 
-if (mysql_errno()>0)
+if (mysql_num_rows($r_root)==0)
 {
     // root not found
     $root = array(
         'name' => 'Root administator',
-        'email' => 'karel.wintersky@gmail.com',
+        'email' => mysql_real_escape_string('karel.wintersky@gmail.com'),
         'permissions' => '255',
         'login' => 'root',
         'password' => 'root',
         'phone' => '',
         'md5password' => md5('root')
     );
-    $q = MakeInsert($post,$root);
-    $r = mysql_query($q,$link) or Die("Unable to insert data to DB!".$qstr);
+    $q = MakeInsert($root,'users');
+    $r = mysql_query($q) or Die("Unable to insert data to DB!   ".$q);
+    echo 'Root user created!';
 } else {
     echo 'Root user found.';
 }
