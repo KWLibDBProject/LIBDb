87a84a3f00043b1ef5aea84d4a597ebf37758e00
KarelWintersky <karel.wintersky@gmail.com>
2014-04-19 15:14:27 +0400

    core { books.insert, books.update - разделение логики добавления в базу информации о файлах и содержимого файлов }

--- www/core/ref_books/books.action.update.php
+++ www/core/ref_books/books.action.update.php
@@ -30,7 +30,6 @@ if (count($_FILES)>0) {
 
     // неважно сколько файлов пришло из формы - обработаем массив с ними в цикле
     foreach ($_FILES as $a_file => $a_file_data) {
-
         $insert_array = array(
             'username' => $a_file_data['name'],
             'tempname' => ($_SERVER['REMOTE_ADDR']==="127.0.0.1") ? str_replace('\\','\\\\', $a_file_data['tmp_name']) : $a_file_data['tmp_name'],
@@ -39,15 +38,25 @@ if (count($_FILES)>0) {
             'filetype' => $a_file_data['type'],
             'collection' => 'books'
         );
-        $insert_array['content'] = mysql_escape_string(floadfile($insert_array['tempname']));
-        //@todo: разделить вставку контента и простых данных на файл. При переезде сторэйджа в
-        //файловое хранение будет достаточно переписать 1 функцию
-
         $q = MakeInsert($insert_array, $ref_filestorage);
-
         mysql_query($q, $link) or Die("Death on $q");
-        $book_file_rels[$a_file] = mysql_insert_id() or Die("Не удалось получить id последнего добавленного файла !");
 
+        $inserted_file_id = mysql_insert_id() or Die("Не удалось получить id последнего добавленного файла !");
+
+        /* Разделяем вставку контента и информации о файле в базу */
+        $insert_content_array = array(
+            'content' => mysql_escape_string(floadfile($insert_array['tempname']))
+        );
+        $qc = MakeUpdate($insert_content_array, $ref_filestorage, " WHERE id = {$inserted_file_id}");
+        mysql_query($qc, $link);
+        /* Конец разделения. Это надо оформлять функцией из filestorage.lib.php
+        и переписывать на хранение в каталоге
+         */
+
+
+        $book_file_rels[$a_file] = $inserted_file_id;
+        // a_file - одно из значений из перечисления идентификаторов файлов из инпута
+        // к примеру $book_file_rels['file_toc'] = id этого файла, только что вставленного в БД
     }
     $q_rels = MakeUpdate($book_file_rels, 'books', " WHERE id=$book_id ");
 
