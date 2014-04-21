029835449c7684f01f74a336d5ec464f7c73c769
KarelWintersky <karel.wintersky@gmail.com>
2014-04-21 19:12:07 +0400

    FRONTEND: { minor optimisation
    }

--- www/tpl/frontend.js
+++ www/tpl/frontend.js
@@ -79,3 +79,40 @@ function setCookie (name, value, expires, path, domain, secure) {
         ((domain) ? "; domain=" + domain : "") +
         ((secure) ? "; secure" : "");
 }
+
+function preloadOptionsList(url) // Загружает данные (кэширование)
+{
+    var ret;
+    $.ajax({
+        url: url,
+        async: false,
+        cache: false,
+        type: 'GET',
+        success: function(data){
+            ret = $.parseJSON(data);
+        }
+    });
+    return ret;
+}
+
+// формирует SELECTOR/OPTIONS list с текущим элементом равным [currentid]
+// target - ИМЯ селектора
+function BuildSelector(target, data, currentid) // currentid is 1 for NEW
+{
+    if (data['error'] == 0) {
+        var _target = "select[name='"+target+"']";
+        $.each(data['data'], function(id, value){
+            $(_target).append('<option value="'+id+'">'+value+'</option>');
+        });
+        var currentid = (typeof currentid != 'undefined') ? currentid : 1;
+        $("select[name="+target+"] option[value="+ currentid +"]").prop("selected",true);
+        // $("select[name="+target+"]").prop('disabled',false);
+    } else {
+        $("select[name="+target+"]").prop('disabled',true);
+    }
+}
+
+function strpos (haystack, needle, offset) {
+    var i = (haystack+'').indexOf(needle, (offset || 0));
+    return i === -1 ? false : i;
+}
