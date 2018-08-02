c9f1b3934cbd40e5967f2d2d731a41f9c2c51d12
Karel <karel.wintersky@gmail.com>
2018-08-02 20:19:35 +0300

    REMASTERED_MYSQLi, v 1.10
    [!] refactoring Module: TOPICS

--- www/core/core.topics/topics.action.list.php
+++ www/core/core.topics/topics.action.list.php
@@ -7,48 +7,20 @@ $res = mysqli_query($mysqli_link, $query) or die('$msg->say("errors/mysqli_query
 
 $ref_numrows = @mysqli_num_rows($res) ;
 
+$topics_list = [];
+
 if ($ref_numrows > 0) {
-    while($ref_record = mysqli_fetch_assoc($res))
+    while($topic_record = mysqli_fetch_assoc($res))
     {
-        $ref_list[ $ref_record['id']  ] = $ref_record;
+        $topics_list[ $topic_record['id']  ] = $topic_record;
     }
 }
 
-$return = <<<REF_TAL_START
-<table border="1" width="100%">
-REF_TAL_START;
+$template_dir = '$/core/core.topics';
+$template_file = "_template.topics.list.html";
 
-$return .= <<<REF_TAL_TH
-    <tr>
-        <th width="5%">№</th>
-        <th>Thematic section</th>
-        <th>Тематический раздел</th>
-        <th>Тематичний розділ</th>
-        <th>Group</th>
-        <th width="7%">Управление</th>
-    </tr>
-REF_TAL_TH;
+$template_data = array(
+    'topics_list' =>  $topics_list
+);
 
-if ($ref_numrows > 0)
-{
-    foreach ($ref_list as $r_id => $row)
-    {
-        $return .= <<<REF_TAL_EACH
-<tr>
-<td class="centred_cell">{$row['id']}</td>
-<td>{$row['title_en']}</td>
-<td>{$row['title_ru']}</td>
-<td>{$row['title_uk']}</td>
-<td class="centred_cell">{$row['rel_group']}</td>
-<td class="centred_cell"><button class="actor-edit button-edit" name="{$row['id']}">Edit</button></td>
-</tr>
-REF_TAL_EACH;
-    }
-} else {
-    $return .= '<tr><td colspan="6">Пока не добавили ни один тематический раздел.</td></tr></table>';
-}
-$return .= <<<REF_TAL_END
-</table>
-REF_TAL_END;
-
-print($return);
+echo \Websun\websun::websun_parse_template_path($template_data, $template_file, $template_dir);
