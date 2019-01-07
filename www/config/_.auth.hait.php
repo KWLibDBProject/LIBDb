0b8975207da0d2a5a12f9e11d3d88d39ead6372a
Karel Wintersky <karel.wintersky@yandex.ru>
2019-01-07 17:53:40 +0200

    v. 1.146 HAIT Configs
    Added HAID Configs

---
+++ www/config/_.auth.hait.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * User: Karel Wintersky
+ * Date: 06.01.2019
+ */
+ 
+return [
+    'cookie:site_language'  =>  'hait_sitelanguage',
+    'auth:cookies'  =>  [
+        'user_is_logged'    =>  'u_hait_is_logged',
+        'user_permissions'  =>  'u_hait_permissions',
+        'user_id'           =>  'u_hait_userid'
+    ],
+    'auth:session'  =>  [
+        'user_is_logged'    =>  'u_hait_is_logged',
+        'user_permissions'  =>  'u_hait_permissions',
+        'user_id'           =>  'u_hait_userid'
+    ]
+];
---
+++ www/config/_.theme.hait.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * User: Karel Wintersky
+ * Date: 06.01.2019
+ */
+
+return [
+    'template_name'     => 'template.hait',
+    'template_dir'      => 'template.hait',
+    'root_page_title'   => 'HAIT.OPU.UA'
+];
+ 
+ 
