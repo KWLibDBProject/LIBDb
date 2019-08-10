df9d23c790f08aa990e2fc97726c2da8d81a93ac
Karel Wintersky <karel.wintersky@yandex.ru>
2019-08-11 02:21:02 +0300

    1.194
    [#] добавлена возможность использовать языко-зависимые названия сборников (books). Режим активируется в настройках темы (`book:use_lang_depended_title`). Если режим выключен - используется только поле `title_en`, а значения полей `title_ru` и `title_ua` устанавливаются равными `title_en`.
    [+] модифицирован SQL-скрипт для обновления БД

--- database/definitions/2019-08-10_5_NEW_BOOK_TITLES_ENGINE.sql
+++ database/definitions/2019-08-10_5_NEW_BOOK_TITLES_ENGINE.sql
@@ -6,3 +6,8 @@ ALTER TABLE `books` CHANGE `title` `title_en` VARCHAR(80);
 -- добавляем после title_en поля title_ru, title_ua типа varchar(80)
 ALTER TABLE `books` ADD COLUMN `title_ru` VARCHAR(80) AFTER `title_en`;
 ALTER TABLE `books` ADD COLUMN `title_ua` VARCHAR(80) AFTER `title_ru`;
+
+-- 3 
+-- обновляем значения новодобавленных полей значениями из title_en (оригинальный title)
+UPDATE `books` SET `title_ru` = `title_en`;
+UPDATE `books` SET `title_ua` = `title_en`;
